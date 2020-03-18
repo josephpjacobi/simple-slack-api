eeb0e9e2b67c466fba2dd5d81d33dd3ef36f92e5
Joe Jacobi <joseph.p.jacobi@gmail.com>
2020-03-18 13:36:27 -0400

    added functionality to delete a message

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use(
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  res.setHeader('Access-Control-Allow-Methods', '*')
   next();
 });
 
